2a044ccbfc881d9ecc8fa83cefc5266d26b744b7
Patrick Macêdo <patrickmacedofeliceo@gmail.com>
2025-12-03 20:05:04 -0400

    Criação Models e Banco de Dados
    Criação dos diretorios necessarios para os Models futuro, sendo inicialmente o modelo de Filme. Em paralelo criado o AppDbContext que estará utilizando SQLite

---
+++ Sistema-Cine/Sistema-Cine/Models/Filme.cs
@@ -0,0 +1,29 @@
+﻿namespace Sistema_Cine.Models;
+
+public class Filme
+{
+    public int Id { get; set; }
+    public int TmdbId { get; set; }
+
+    public string Titulo { get; set; }
+    public string TituloOriginal { get; set; }
+    public string Sinopse { get; set; }
+    public DateTime? DataLancamento { get; set; }
+
+    public string Genero { get; set; }
+    public string PosterPath { get; set; }
+    public string Lingua { get; set; }
+    public int? Duracao { get; set; }
+    public double? NotaMedia { get; set; }
+
+    public string ElencoPrincipal { get; set; }
+
+    // Dados da cidade (da API Open-Meteo)
+    public string CidadeReferencia { get; set; }
+    public double? Latitude { get; set; }
+    public double? Longitude { get; set; }
+
+    // Metadados para logs no futuro
+    public DateTime DataCriacao { get; set; } = DateTime.UtcNow;
+    public DateTime DataAtualizacao { get; set; } = DateTime.UtcNow;
+}
