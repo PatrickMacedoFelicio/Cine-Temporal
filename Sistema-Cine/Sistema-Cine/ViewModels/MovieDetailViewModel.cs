a5c8d337ddf4de177df8c352184c2f51c88f4dd1
Samuel Felipe <samuel.felipe04004@gmail.com>
2025-12-07 15:27:19 -0400

    Inicio do codigo front end
    definição de views e problemas na execução dos projetos

---
+++ Sistema-Cine/Sistema-Cine/ViewModels/MovieDetailViewModel.cs
@@ -0,0 +1,5 @@
+﻿public class MovieDetailViewModel
+{
+    public MovieViewModel Movie { get; set; }
+    public IEnumerable<WeatherDayViewModel> Forecast { get; set; }
+}
---
+++ Sistema-Cine/Sistema-Cine/wwwroot/js/movies.js
@@ -0,0 +1,7 @@
+﻿document.addEventListener('click', function(e){
+    if(e.target.matches('form button[type="submit"]') && e.target.closest('form')?.getAttribute('asp-action') === 'Import'){
+        if(!confirm('Importar este filme para o catálogo local?')) {
+            e.preventDefault();
+        }
+    }
+});
