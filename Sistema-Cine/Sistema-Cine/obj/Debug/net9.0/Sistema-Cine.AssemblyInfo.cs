ebba37589ceff0bda7cd469877117ef97f671166
Semeler <133254072+Semeler@users.noreply.github.com>
2025-12-10 17:47:44 -0400

    Teste

--- Sistema-Cine/Sistema-Cine/obj/Debug/net9.0/Sistema-Cine.AssemblyInfo.cs
+++ Sistema-Cine/Sistema-Cine/obj/Debug/net9.0/Sistema-Cine.AssemblyInfo.cs
@@ -15,7 +15,11 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed8f4ad72e6a268e19084bdf21388c52daeba462")]
+=======
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+83bf71408d58930891042f3ef8fd11644af94ccd")]
+>>>>>>> Stashed changes
 =======
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+83bf71408d58930891042f3ef8fd11644af94ccd")]
 >>>>>>> Stashed changes
